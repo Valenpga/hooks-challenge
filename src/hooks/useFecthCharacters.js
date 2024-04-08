7c558c7ea99f87ea967b461e71247654138c9b05
Valentín Peñalver <valenpga@gmail.com>
2024-04-08 22:11:43 +0200

    hoock terminado

--- src/hooks/useFecthCharacters.js
+++ src/hooks/useFecthCharacters.js
@@ -1,1 +1,23 @@
+import { useState, useEffect } from "react"; 
 
+function useCustomHook (apiURL)  {
+    const [data, setData] = useState()
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try{
+                const response = await fetch(apiURL);
+                if(!response.ok)
+                throw new Error ('No encuentra imformacion');
+
+            } catch (error) {
+                console.error('Datos no encontrados',error);
+            } 
+            const fetchData = response.json();
+            setData();
+        }
+        fetchData()
+    }, [apiURL])
+    return data;
+}
+export default useCustomHook;
